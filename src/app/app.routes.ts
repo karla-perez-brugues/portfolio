89b68dfc4012f3b4091521cc9ce3c985ebafb5be
Karla PEREZ <karla.perezbrugues@gmail.com>
2024-10-31 23:34:25 +0100

    add routes for hero and experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HeroComponent} from './hero/hero.component';
+import {ExperienceComponent} from './experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeroComponent},
+  { path: 'experience', component: ExperienceComponent},
+];
